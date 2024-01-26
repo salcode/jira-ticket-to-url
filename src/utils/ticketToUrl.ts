8c2ff62192db673630050509fc374f9119f6bdd4
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2024-01-26 14:12:39 -0500

    Add utility ticketToUrl

---
+++ src/utils/ticketToUrl.ts
@@ -0,0 +1,12 @@
+import {
+    getPreferenceValues,
+} from "@raycast/api";
+
+interface Preferences {
+  baseurl: string;
+}
+
+export default function ticketToUrl(ticket) {
+  const { baseurl } = getPreferenceValues<Preferences>();
+  return `${baseurl}browse/${ticket}`
+}
