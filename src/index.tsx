1f8019440920112a6bd13340868ecd436fbdd4ef
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2024-01-26 10:51:40 -0500

    Delete unnecessary fields

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,6 @@ import { Form, ActionPanel, Action, showToast } from "@raycast/api";
 
 type Values = {
   textfield: string;
-  textarea: string;
-  datepicker: Date;
-  checkbox: boolean;
-  dropdown: string;
-  tokeneditor: string[];
 };
 
 export default function Command() {
@@ -25,16 +20,6 @@ export default function Command() {
     >
       <Form.Description text="This form showcases all available form elements." />
       <Form.TextField id="textfield" title="Text field" placeholder="Enter text" defaultValue="Raycast" />
-      <Form.TextArea id="textarea" title="Text area" placeholder="Enter multi-line text" />
-      <Form.Separator />
-      <Form.DatePicker id="datepicker" title="Date picker" />
-      <Form.Checkbox id="checkbox" title="Checkbox" label="Checkbox Label" storeValue />
-      <Form.Dropdown id="dropdown" title="Dropdown">
-        <Form.Dropdown.Item value="dropdown-item" title="Dropdown Item" />
-      </Form.Dropdown>
-      <Form.TagPicker id="tokeneditor" title="Tag picker">
-        <Form.TagPicker.Item value="tagpicker-item" title="Tag Picker Item" />
-      </Form.TagPicker>
     </Form>
   );
 }
